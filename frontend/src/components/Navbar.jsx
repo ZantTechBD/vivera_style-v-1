a8b1550427198db3305422d10abb504bc928dff9
ZantTechBD <zanttechbd04@gmail.com>
2024-11-24 22:30:32 +0600

    test push

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
         <NavLink to='/about' className='flex flex-col items-center gap-1'>
-            <p>ABOUT</p>
+            <p>ABOUT US</p>
             <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
         </NavLink>
         <NavLink to='/contact' className='flex flex-col items-center gap-1'>
